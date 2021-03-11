9dc0703fc440049283faf864d90d1c284a84f612
Letícia Oliveira <leticia164_@hotmail.com>
2021-03-11 18:32:55 -0300

    Só falta adicionar o logout e deixar binitão

--- Front/js/transactions.js
+++ Front/js/transactions.js
@@ -8,21 +8,30 @@ function carregarParceiro(){
         // Obter novamente as informações daquele parceiro
         var parceiroJson = JSON.parse(parceiro);
 
-        var cabecalho = {
-            method: "GET"
-        }
-
-        fetch("http://localhost:8080/agente/" + parceiroJson.id, cabecalho)
+        fetch("http://localhost:8080/agente/" + parceiroJson.id)
         .then(res => res.json())
         .then(res => {
             // Preencher o card de parceiro
             document.getElementById("parceiro").innerHTML = res.nomeAgente + " / " + res.volumeTransacional
         })
         .catch(err => {
-            window.alert("Não foi possível obter as informações desejadas");
+            window.alert("Não foi possível obter as informações do parceiro");
         });
 
 
         // Obter as infos dos dashs
+        fetch("http://localhost:8080/totalTransacao/" + parceiroJson.id)
+        .then(res => res.json())
+        .then(res => {
+            // Preencher o card de parceiro
+            document.getElementById("sucessos").innerHTML = res[0];
+            document.getElementById("falhas").innerHTML = res[1];
+            document.getElementById("fraudes").innerHTML = res[2];
+        })
+        .catch(err => {
+            window.alert("Não foi possível obter as informações das transações");
+        });
+
+
     }
 }
